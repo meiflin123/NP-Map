f771e6e2f3fb6c481368c2f11385ef7dddeca388
Meifang Lin <meiflin123@gmail.com>
2019-06-07 11:23:14 -0700

    install google-maps-react and load google map to component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { GOOGLE_MAP_KEY,NATIONAL_PARK_SERVICE_KEY } from '../config/api';
 
-
-const App = () => {
-
-  return <div>App</div>
+const mapStyles = {
+  width: '100%',
+  height: '100%'
 };
 
-export default App;
+export class App extends React.Component {
+
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: 37.774929,
+         lng: -122.419416
+        }}
+      />
+    );
+  }
+
+
+};
+
+export default GoogleApiWrapper({
+  apiKey: GOOGLE_MAP_KEY
+})(App);
+
