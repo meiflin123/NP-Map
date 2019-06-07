c336fe849059d1b9af96a87351a4e1ea7b461e76
Meifang Lin <meiflin123@gmail.com>
2019-06-07 11:41:44 -0700

    render Marker and InfoWindow

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import { GOOGLE_MAP_KEY,NATIONAL_PARK_SERVICE_KEY } from '../config/api';
 
 const mapStyles = {
@@ -9,20 +9,57 @@ const mapStyles = {
 
 export class App extends React.Component {
 
+  state = {
+    showingInfoWindow: false,  
+    activeMarker: {},       
+    selectedPlace: {} 
+  }
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    })
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
+
   render() {
     return (
       <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
+        google={ this.props.google }
+        zoom={ 14 }
+        style={ mapStyles }
         initialCenter={{
          lat: 37.774929,
          lng: -122.419416
         }}
-      />
+      >
+
+        <Marker
+          onClick={ this.onMarkerClick }
+          name={ 'San Francisco' }
+
+        />
+        <InfoWindow
+              marker={ this.state.activeMarker }
+              visible={ this.state.showingInfoWindow }
+              onClose={ this.onClose }
+        >
+          <div>
+            <h4>{ this.state.selectedPlace.name }</h4>         
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
-
 
 };
 
