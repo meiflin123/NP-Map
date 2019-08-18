22b3eeef73cdee739e41ddd718ab52daec69c84f
Meifang Lin <meiflin123@gmail.com>
2019-08-17 17:56:27 -0700

    update auth state by sign in sign out

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,6 +3,9 @@ import React from 'react';
 
 class GoogleAuth extends React.Component {
 
+  state = {
+    isSignedIn: null
+  }
 
   componentDidMount(){
     window.gapi.load('client:auth2', () => {
@@ -11,15 +14,27 @@ class GoogleAuth extends React.Component {
         scope: 'email'
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
-        console.log(this.auth)
-
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     })
 
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  }
+  renderAuthButton() {
+    if(this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>
+    } else {
+      return <div>I am not signed in</div>
+    }
+  }
   render () {
-    return <div>GoogleAuth</div>
+    return <div>{ this.renderAuthButton() }</div>
   }
 
 
