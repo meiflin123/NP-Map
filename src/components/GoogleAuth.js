10b5f3ad9b9cd8158f10fb4eff66189476794eb2
Meifang Lin <meiflin123@gmail.com>
2019-08-17 22:43:03 -0700

    handle onClick for sign in and sign out button

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -24,19 +24,27 @@ class GoogleAuth extends React.Component {
   onAuthChange = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   }
+
+  onSignIn = () => {
+    this.auth.signIn();
+  }
+
+  onSignOut = () => {
+    this.auth.signOut();
+  }
   renderAuthButton() {
     if(this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui google button">
+        <button className="ui google button" onClick={ this.onSignOut }>
           <i className="google icon" />
           Sign Out
         </button>
       )
     } else {
       return (
-        <button className="ui google button">
+        <button className="ui google button" onClick={ this.onSignIn }>
           <i className="google icon" />
           Sign In
         </button>
